3a4fa3f9c955ae8fd2271032f23ebdfc668dbc68
LiangYi <994302767@qq.com>
2022-04-09 02:51:23 +0800

    feat(测试): 添加了Benchmark和Benchmark示例

---
+++ src/main/java/com/edison/benchmark/ListTest.java
@@ -0,0 +1,73 @@
+package com.edison.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author LiangYi
+ * @data 2022/4/9
+ * @note
+ */
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+@Fork(2)
+@Warmup(iterations = 4)
+@Measurement(iterations = 10)
+@SuppressWarnings("all")
+public class ListTest {
+
+    @Param(value = {"10", "100", "1000"})
+    private int n;
+
+    private List<Integer> arrayList;
+    private List<Integer> linkedList;
+
+    @Setup(Level.Trial)
+    public void init() {
+        arrayList = new ArrayList<>(0);
+        linkedList = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+    }
+
+    @Benchmark
+    public void arrayTraverse() {
+        for (int i = 0; i < n; i++) {
+            arrayList.get(i);
+        }
+    }
+
+    @Benchmark
+    public void listTraverse() {
+        for (int i = 0; i < n; i++) {
+            linkedList.get(i);
+        }
+    }
+
+    @TearDown(Level.Trial) // 结束方法，在全部Benchmark运行之后进行
+    public void arrayRemove() {
+        for (int i = 0; i < n; i++) {
+            arrayList.remove(0);
+            linkedList.remove(0);
+        }
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder().include(ListTest.class.getSimpleName()).build();
+        new Runner(options).run();
+    }
+
+
+}
