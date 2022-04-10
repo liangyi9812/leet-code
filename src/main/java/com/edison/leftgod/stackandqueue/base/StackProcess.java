538ca28c2b0203db1bcf499bb3447e8c3f1dda89
LiangYi <994302767@qq.com>
2022-04-10 23:30:12 +0800

    add

--- src/main/java/com/edison/leftgod/stackandqueue/base/StackProcess.java
+++ src/main/java/com/edison/leftgod/stackandqueue/base/StackProcess.java
@@ -1,5 +1,7 @@
 package com.edison.leftgod.stackandqueue.base;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -9,6 +11,12 @@ import java.util.Stack;
  */
 public class StackProcess {
 
+    /**
+     * 使用一个栈实现栈排序
+     * @param stackToSort
+     * @param <T>
+     * @return Stack<T>
+     */
     public static <T extends Comparable<T>>Stack<T> sortStack(Stack<T> stackToSort) {
         Stack<T> stack = new Stack<>();
         if (stackToSort.isEmpty()) {
@@ -24,6 +32,28 @@ public class StackProcess {
             stack.push(pop);
         }
         return stack;
+    }
+
+    public static List<Integer> findWindowMaxRes(int[] arrays, int windowSize) {
+        List<Integer> res = new ArrayList<>();
+        if (windowSize == 0) {
+            return res;
+        }
+        int startIndex = 0;
+        for (int i = 0; i < arrays.length; i++) {
+            int endIndex = startIndex + windowSize - 1;
+            if (endIndex > arrays.length - 1) {
+                // 窗口尾部超出数组大小
+                break;
+            }
+            int max = Integer.MIN_VALUE;
+            for (int j = startIndex; j <= endIndex; j++) {
+                max = Math.max(max, arrays[j]);
+            }
+            startIndex++;
+            res.add(max);
+        }
+        return res;
     }
 
 }
--- src/test/java/com/edison/leftgod/stackandqueue/base/StackProcessTest.java
+++ src/test/java/com/edison/leftgod/stackandqueue/base/StackProcessTest.java
@@ -2,9 +2,11 @@ package com.edison.leftgod.stackandqueue.base;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author LiangYi
@@ -22,5 +24,15 @@ class StackProcessTest {
         integerStack.push(1);
         Stack<Integer> sort = StackProcess.sortStack(integerStack);
         System.out.println("done! " + sort);
+    }
+
+    @Test
+    void findWindowMaxRes() {
+        int[] intData = {4, 3, 5, 4, 3, 3, 6, 7};
+        int windowSize = 3;
+        List<Integer> maxRes = StackProcess.findWindowMaxRes(intData, 3);
+
+        assertEquals(intData.length - windowSize + 1, maxRes.size());
+        maxRes.forEach(System.out::println);
     }
 }
